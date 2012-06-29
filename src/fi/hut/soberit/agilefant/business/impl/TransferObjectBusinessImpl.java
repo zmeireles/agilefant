9b0b6c458e07d7387836960a3e43a1c877332334
Benjamin Behm <benjamin.behm@aalto.fi>
2012-06-29 12:02:46 +0300

    Undo #1180 changes

--- src/fi/hut/soberit/agilefant/business/impl/TransferObjectBusinessImpl.java
+++ src/fi/hut/soberit/agilefant/business/impl/TransferObjectBusinessImpl.java
@@ -134,23 +134,7 @@ public class TransferObjectBusinessImpl implements TransferObjectBusiness {
     /** {@inheritDoc} */
     @Transactional(readOnly = true)
     public List<AutocompleteDataNode> constructUserAutocompleteData() {
-        User loggedUser = SecurityUtil.getLoggedUser();
-        Collection<User> allUsers = null;
-        if (loggedUser.isAdmin()) {
-            allUsers = this.userBusiness.retrieveAll();
-        }   else {
-            Collection<Team> t = loggedUser.getTeams();
-            for(Team team : t) {
-                Collection<User> u = team.getUsers();
-                for (User user : u) {
-                    if (allUsers.contains(user)) {
-                        allUsers.add(user);
-                    }
-                }
-                u.clear();
-            }
-            t.clear();
-        }
+        Collection<User> allUsers = this.userBusiness.retrieveAll();
         List<AutocompleteDataNode> autocompleteData = new ArrayList<AutocompleteDataNode>();
         for(User user : allUsers) {
             AutocompleteDataNode curNode = new AutocompleteDataNode(User.class,
@@ -165,25 +149,21 @@ public class TransferObjectBusinessImpl implements TransferObjectBusiness {
     /** {@inheritDoc} */
     @Transactional(readOnly = true)
     public List<AutocompleteDataNode> constructTeamAutocompleteData(boolean listUserIds) {
-        User loggedUser = SecurityUtil.getLoggedUser();
         Collection<Team> allTeams = this.teamBusiness.retrieveAll();
         List<AutocompleteDataNode> autocompleteData = new ArrayList<AutocompleteDataNode>();
-        Collection<Team> teams= loggedUser.getTeams();
         for(Team team : allTeams) {
-            if (teams.contains(team)) {
-                Set<Integer> userIds = null;
-                if (listUserIds) {
-                    userIds = new HashSet<Integer>();
-                    for (User user : team.getUsers()) {
-                        userIds.add(user.getId());
-                    }
+            Set<Integer> userIds = null;
+            if (listUserIds) {
+                userIds = new HashSet<Integer>();
+                for(User user : team.getUsers()) {
+                    userIds.add(user.getId());
                 }
-                AutocompleteDataNode curNode = new AutocompleteDataNode(Team.class,
-                        team.getId(), team.getName(), userIds);
-                curNode.setMatchedString(team.getName());
-                curNode.setOriginalObject(team);
-                autocompleteData.add(curNode);
             }
+            AutocompleteDataNode curNode = new AutocompleteDataNode(Team.class,
+                    team.getId(), team.getName(), userIds);
+            curNode.setMatchedString(team.getName());
+            curNode.setOriginalObject(team);
+            autocompleteData.add(curNode);
         }
         return autocompleteData;
     }
