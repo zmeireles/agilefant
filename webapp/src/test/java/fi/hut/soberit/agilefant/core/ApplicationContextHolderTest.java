29789bd876ddfb9808e72a30f1f2c69c388d8c61
Joonas Javanainen <joonas.javanainen@gmail.com>
2013-01-30 11:02:58 +0200

    Fix: Invalid Spring XML locations in a test

--- webapp/src/test/java/fi/hut/soberit/agilefant/core/ApplicationContextHolderTest.java
+++ webapp/src/test/java/fi/hut/soberit/agilefant/core/ApplicationContextHolderTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:conf/applicationContext-core.xml", "file:conf/applicationContext-notifications.xml" })
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext-core.xml", "file:src/main/webapp/WEB-INF/applicationContext-notifications.xml" })
 public class ApplicationContextHolderTest {
 
     private List<NotificationConfiguration> notificationsConfiguration;
