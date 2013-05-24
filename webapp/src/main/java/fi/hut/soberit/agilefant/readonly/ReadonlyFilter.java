37376f6d9d4ae14dd270fbdf2f70c8640e1edb13
jkorri <santeri.korri@iki.fi>
2013-05-24 14:00:07 +0300

    Extracted TokenFilter from ReadonlyFilter

--- webapp/src/main/java/fi/hut/soberit/agilefant/readonly/ReadonlyFilter.java
+++ webapp/src/main/java/fi/hut/soberit/agilefant/readonly/ReadonlyFilter.java
@@ -1,97 +1,40 @@
 package fi.hut.soberit.agilefant.readonly;
 
 import java.io.IOException;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.filter.GenericFilterBean;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.Session;
-
-import fi.hut.soberit.agilefant.db.hibernate.IterationDAOHibernate;
-import fi.hut.soberit.agilefant.db.hibernate.UserDAOHibernate;
-
+import fi.hut.soberit.agilefant.business.IterationBusiness;
 
 public class ReadonlyFilter extends GenericFilterBean {
-    
-    @Transactional(readOnly = true)
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        
-        // Our passed-in requests and responses are HttpServlet requests and responses.
-        HttpServletResponse resp = (HttpServletResponse) response;
-        HttpServletRequest reqt = (HttpServletRequest) request;
-        
-        // Create a Data Access Object instance and open a Hibernate session.
-        IterationDAOHibernate iterationDao = new IterationDAOHibernate();
-        UserDAOHibernate userDao = new UserDAOHibernate();
-        
-        SessionFactory sessionFactory;
-        try {
-            sessionFactory = (SessionFactory) new InitialContext().lookup("hibernateSessionFactory");
-            iterationDao.setSessionFactory(sessionFactory);
-            userDao.setSessionFactory(sessionFactory);
-        } catch (NamingException e) {
-            e.printStackTrace();
-            return;
-        }
-        Session session = sessionFactory.openSession();
-        
-        // Fetch url token from request.
-        String requestUrl = reqt.getRequestURL().toString();
-        String requestUri = reqt.getRequestURI().toString();
-        String token = getTokenFromUrl(requestUrl);
-        String readonlyToken = reqt.getParameter("readonlyToken");
-        session.disconnect();
-        session.close();
-        
-        if (iterationDao.isValidReadonlyToken(token)) {
-            resp.sendRedirect(getAgilefantPathFromURI(requestUri) + "/ROIteration.action?readonlyToken=" + token);
-            
-        } else if (requestUrl.contains("ROIteration") && (!requestUrl.endsWith("ROIteration.action") || iterationDao.isValidReadonlyToken(readonlyToken))) {
-            
-            // I don't think this is needed but I am leaving it here for now, 
-            // in case removing it does break something. - Dustin, Mar-21-2012
-            //User user = userDao.getByLoginName("readonly");
-            //SecurityUtil.setLoggedUser(user);
-            
-            try{
-                chain.doFilter(request, response);
-            } catch(NullPointerException npe){
-                //user tried to go back in the browser after unsharing
-                resp.sendRedirect(getAgilefantPathFromURI(requestUri) + "/login.jsp");
-            }
-        } else {
-            // Token is not valid, so redirect to login page.
-            resp.sendRedirect(getAgilefantPathFromURI(requestUri) + "/login.jsp");
-        }
-    }
-    
-    private String getTokenFromUrl(String url) {
-        if(url != null) {
-            int tokenStart = url.indexOf("token/");
-            String token = url.substring(tokenStart + "token/".length());
-            return token;
-        }
-        else 
-            return "";
-    }
-    
-    private String getAgilefantPathFromURI(String uri) {
-        if (uri != null && uri.indexOf("/", 1) != -1) {
-            String loginPath = uri.substring(0, uri.indexOf("/", 1));
-            return loginPath;
-        } else {
-            return "/agilefant";
-        }
-    }
 
+	private static final String READONLYTOKEN_PARAM = "readonlyToken";
+	
+	private static final String READONLY_ACTION_PATTERN = ".*/RO[^/]*\\.action$";
+	
+	@Autowired
+	IterationBusiness iterationBusiness;
+	
+    @Transactional(readOnly = true)
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		String readonlyToken = request.getParameter(READONLYTOKEN_PARAM);
+		
+		if(readonlyToken!=null) {
+			boolean iterationExists = this.iterationBusiness.getIterationCountFromReadonlyToken(readonlyToken) > 0;
+			boolean actionIsReadOnly = ((HttpServletRequest)request).getRequestURL().toString().matches(READONLY_ACTION_PATTERN);
+			if(iterationExists && actionIsReadOnly) {
+				chain.doFilter(request, response);
+				return;
+			}
+		}
+
+        throw new InvalidReadOnlyAccessException("Invalid read only access");
+    }
 }
